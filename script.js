20d53fcb88d0c97dcad294ee8d70d091c26045ad
Magooch <crvallecito@gmail.com>
2024-07-14 19:59:34 -0600

    Add more sections from Peak and create new variable of "sections_read"

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ const sections_default = ["Introduction: The Gift", "The Lesson of Perfect Pitch
     "The Limits of Purposeful Practice", "Harnessing Adaptability", "The Brains of London Cabbies", "Adaptability", "Challenging Homeostasis", "Shaping The Brain", "Building Your Own Potential", "Mental Representations",
     "The Accidental Blindfold Chess Master", "The Secret To Winning Chess", "Mental Representations", "Recognizing and Responding To Patterns", "Making Sense of Information", "Finding an Answer", "Planning",
     "Mental Representations in Learning", "Physical Activities are Mental Too", "The Gold Standard", "A Highly Developed Field", "The Challenge of the Violin", "Good Versus Better Versus Best", 
-    "The Principles of Deliberate Practice", "Applying the Principles of Deliberate Practice", "No, The Ten-Thousand-Hour Rule isn't Really a Rule"]
+    "The Principles of Deliberate Practice", "Applying the Principles of Deliberate Practice", "No, The Ten-Thousand-Hour Rule isn't Really a Rule", "Principles of Deliberate Practice on the Job", "Practicing While Getting Work Done",
+    "The Top Gun Approach to Learning", "Knowledge Versus Skills", "A New Approach to Training", "Principles of Deliberate Practice in Everyday Life", "First, Find a Good Teacher", "Engagement", "If You Don't Have a Teacher",
+    "Getting Past Plateus", "Maintaining Motivation", "The Road to Extraordinary", "Starting Out", "Becoming Serious", "Commitment", "The Benefits of Starting Young", "More Lessons from Perfect Pitch", "Pathbreakers",
+    "But What About Natural Talent", "The Magic of Paganini", "Mozart and His Legend", "The Magical High Jumper", "Savants", "The Anti-Prodigies", "Practice Versus \"Talent\" in Chess", "The Real Role of Innate Characteristics",
+    "The Dark Side of Believing in Innate Talent", "Where Do We Go From Here?", "The Promise of Deliberate Practice", "Homo Exercens"]
+
+const sections_read = ["The Top Gun Approach to Learning"]
+
